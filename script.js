ef256771a23fe584c3aa9457be017fbca6ba8109
Joel Foubia <joelngamba47@gmail.com>
2023-01-31 15:56:04 +0100

    Modification du fichier script.js côté front end

---
+++ script.js
@@ -0,0 +1,5 @@
+// Challenge 5 blackjack 
+document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+function blackjackHit(){
+    alert('Hey! You just clicked me.');
+}
